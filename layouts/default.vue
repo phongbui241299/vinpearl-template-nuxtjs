ee4ec0ad99ae971ab40e6090194a81889c574ee5
phongbui241299 <phongbui241299@gmail.com>
2021-01-31 10:04:06 +0700

    dev: config

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app light>
+
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+    }
+  }
+}
+</script>
